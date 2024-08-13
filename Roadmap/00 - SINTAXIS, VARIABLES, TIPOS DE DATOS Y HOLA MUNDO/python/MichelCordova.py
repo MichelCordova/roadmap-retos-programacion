643a94a20ef39570c2ba0b076ded9c85cf707e63
Michel Cordova <cordovamichel540@gmail.com>
2024-08-12 21:16:59 -0600

    #00-python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/MichelCordova.py
@@ -0,0 +1,40 @@
+# https://www.python.org/
+
+# Comentario en una linea   
+
+'ESTE ES UN COMENTARIO EN PYTHON'
+
+"""
+Este tambien es un 
+comentario en varias 
+lineas!!!!!!
+"""
+
+'''
+Tambien se puede poner un
+comentario con comillas simples
+'''
+
+#Crea una variable (y una constante si el lenguaje lo soporta).
+
+mi_variable = 'Hola estoy aqui'
+TELEFONO_MOVIL = 5655544525
+
+# Crea variables representando todos los tipos de datos primitivos *   del lenguaje (cadenas de texto, enteros, booleanos...).
+
+my_str = 'Hola'
+my_int = 45
+my_float = 3.1416
+my_boolean = True
+my_boolean = False
+
+
+#  Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print('¡Hola, Python !')
+
+
+print(type(my_str))
+print(type(my_int))
+print(type(my_float))
+print(type(my_boolean))
