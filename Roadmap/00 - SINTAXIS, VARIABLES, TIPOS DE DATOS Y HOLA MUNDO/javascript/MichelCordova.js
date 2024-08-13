27223bd493e0b825398cca411843eb4ac89708e6
Michel Cordova <cordovamichel540@gmail.com>
2024-08-12 21:18:10 -0600

    #00-JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/MichelCordova.js
@@ -0,0 +1,33 @@
+// Este es un comentario en JavaScript en una sola linea
+
+/* Este es un comentario 
+en varias 
+lineas */
+
+
+//Crea una variable (y una constante si el lenguaje lo soporta).
+
+let variable = 'miVariable'
+const PI = 3.1416
+
+// Crea variables representando todos los tipos de datos primitivos *   del lenguaje (cadenas de texto, enteros, booleanos...).
+
+let entero = 1
+let flotante = 16.15
+let boleano = false
+let boleano2 = true
+let myString = 'Hola a todos'
+
+//   Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!
+
+const saludo = 'JavaScript'
+
+console.log(`¡Hola, ${saludo}!`)
+
+
+console.log(typeof(entero))
+console.log(typeof(flotante))
+console.log(typeof(boleano))
+console.log(typeof(boleano2))
+console.log(typeof(myString))
+
